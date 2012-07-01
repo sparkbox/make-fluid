a19cff10ac122a5ca7b2d9b15155e9b12714bc38
Ben Callahan <ben@heysparkbox.com>
2012-07-01 08:00:12 -0400

    changing width

--- make-fluid.js
+++ make-fluid.js
@@ -3,7 +3,7 @@
 var all = document.getElementsByTagName("*");
 
 for (var i=0, max=all.length; i < max; i++) {
-  var parentWidth = all[i].pixelWidth;
+  var parentWidth = all[i].style.width;
   if (i==10) {
     alert(parentWidth);
   }
