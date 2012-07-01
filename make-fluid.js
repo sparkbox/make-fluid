8cad21dbd5500517474c091fd280fe0e845eca9c
Ben Callahan <ben@heysparkbox.com>
2012-06-30 23:25:58 -0400

    mods

--- make-fluid.js
+++ make-fluid.js
@@ -1,8 +1,8 @@
-document.body.setAttribute("style", "background-color: red;");
+// document.body.setAttribute("style", "background-color: red;");
 
 var all = document.getElementsByTagName("*");
 
 for (var i=0, max=all.length; i < max; i++) {
   var parentWidth = all[i].width();
-  document.body.appendChild("<div>" + parentWidth + "</div>");
+  document.body.appendChild(parentWidth);
 }
