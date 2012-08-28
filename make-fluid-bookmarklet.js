413976f00577d0c1522ce9a193e6524eb754da20
Ben Callahan <ben@heysparkbox.com>
2012-08-27 22:49:12 -0400

    modifying bookmarklet

--- make-fluid-bookmarklet.js
+++ make-fluid-bookmarklet.js
@@ -1,8 +1,5 @@
 javascript: (function () {
-  // var jquery = document.createElement('script');
-  // jquery.setAttribute('src', 'http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js');
-  // document.body.appendChild(jquery);
   var jsCode = document.createElement('script');
-  jsCode.setAttribute('src', 'https://raw.github.com/bencallahan/make-fluid/master/make-fluid.js');
+  jsCode.setAttribute('src', 'http://sparkbox.github.com/make-fluid/make-fluid.js');
   document.body.appendChild(jsCode);
  }());
